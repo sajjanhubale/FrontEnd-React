b0d60938a75d591297092f61f0274f68c3ea4b7d
BCS <helpdesk@beeceeyes.com>
2019-02-13 21:47:22 +1100

    list_and_delete_contacts

---
+++ src/JS/url.js
@@ -0,0 +1,5 @@
+const serverUrl = `http://${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}/`;
+export default {
+    serverUrl:serverUrl
+};
+
--- src/components/MainContactsComp.js
+++ src/components/MainContactsComp.js
@@ -1,39 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Card, CardHeader,CardFooter, CardBody, ListGroup, ListGroupItem, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Card, CardHeader, CardFooter, CardBody, ListGroup, ListGroupItem, Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input, Label } from 'reactstrap';
 import { mapDispatchToProps } from "../reducers/contact_reducer";
+import axios from "axios";
+import url from '../JS/url.js';
+import swal from 'sweetalert';
 class MainContactsComp extends Component {
     constructor() {
         super();
         this.state = {
             modalForVUD: false,
             selectedContact: null,
+            contact_name: null,
+            contact_mobile_number: null,
             contacts: []
         }
         this.toggleForVUD = this.toggleForVUD.bind(this);
         this.toggleForNew = this.toggleForNew.bind(this);
         this.onContactView = this.onContactView.bind(this);
+        this.addNewContact = this.addNewContact.bind(this);
+        this.loadContactList = this.loadContactList.bind(this);
+        this.deleteContact = this.deleteContact.bind(this);
     }
     componentDidMount() {
-        let contacts = [
-            {
-                name: "ABC",
-                mobile_number: "81375979235"
-            },
-            {
-                name: "PQR",
-                mobile_number: "9813457905"
-            },
-            {
-                name: "MNS",
-                mobile_number: "9835850253"
-            },
-            {
-                name: "XYZ",
-                mobile_number: "9845720500235"
+        this.loadContactList();
+    }
+
+    loadContactList() {
+        axios.post(url.serverUrl + 'contacts/getlistofcontacts').then((response) => {
+            if (response.data.flag === "success") {
+             this.setState({contacts:response.data.data})
             }
-        ]
-        this.setState({ contacts: contacts })
+            else
+              swal("Sorry!!", "Failed to load contacts.");
+          }).catch(function (err) {
+            swal("Sorry!!", "Load Contacts - Something went wrong with server");
+          })
     }
     toggleForVUD() {
         this.setState(prevState => ({
@@ -52,16 +54,47 @@ class MainContactsComp extends Component {
         })
 
     }
-    onNewContact = (e) =>{
-            this.toggleForNew();
+    onNewContact = (e) => {
+        this.toggleForNew();
     }
-    onContactView = () =>{
+    onContactView = () => {
         this.toggleForNew();
         let selectedContact = this.state.contacts.filter(item => item.name === this.state.selectedContact);
         this.props.setSelectedContact(selectedContact[0]);
         this.props.history.push("/contactdetails");
-     
-     
+    }
+    addNewContact = () => {
+        this.setState({modalForNew:false,contact_name:null,mobileNumber:null});
+        let jsonData = {
+            name:this.state.contact_name,
+            mobileNumber:this.state.contact_mobile_number
+        }
+        axios.post(url.serverUrl + 'contacts/addnewcontact', jsonData).then((response) => {
+            if (response.data.flag === "success") {
+                this.loadContactList();
+              swal("Success!!", "New contact added Successfully");
+            }
+            else
+              swal("Sorry!!", "Failed to add new contact.");
+          }).catch(function (err) {
+            swal("Sorry!!", "Add new contact -Something went wrong with server.");
+          })
+    }
+    deleteContact = () => {
+        this.setState({modalForVUD:false});
+        let jsonData = {
+            name:this.state.selectedContact
+        }
+        axios.post(url.serverUrl + 'contacts/deletecontact', jsonData).then((response) => {
+            if (response.data.flag === "success") {
+                this.loadContactList();
+              swal("Success!!", "Selected contact deleted Successfully");
+            }
+            else
+              swal("Sorry!!", "Failed to delete contact.");
+          }).catch(function (err) {
+            swal("Sorry!!", "Delete contact - Something went wrong with server.");
+          })
     }
     render() {
         return (
@@ -74,31 +107,40 @@ class MainContactsComp extends Component {
                         <ListGroup>
                             {
                                 this.state.contacts.map(item => {
-                                    return <ListGroupItem id={item.name} onClick={this.onContactClick}>{item.name}</ListGroupItem>
+                                    return <ListGroupItem id={item.name} key={item.name} onClick={this.onContactClick}>{item.name}</ListGroupItem>
                                 })
                             }
                         </ListGroup>
                     </CardBody>
                     <CardFooter>
-                    <button id="addcontact" onClick={this.onNewContact} style={{ "margin-left": "15px", float: "left" }} ><i style={{ color: 'black' }} className="fa fa-plus" ></i> New</button>
+                        <button id="addcontact" onClick={this.onNewContact} style={{ marginLeft: "15px", float: "left" }} ><i style={{ color: 'black' }} className="fa fa-plus" ></i> New</button>
                     </CardFooter>
                     <Modal isOpen={this.state.modalForVUD} toggle={this.toggleForVUD} className={this.props.className}>
                         <ModalHeader toggle={this.toggleForVUD}>{this.state.selectedContact}</ModalHeader>
                         <ModalBody>
+                            do you want to View/Edit/Delete this contact?
                         </ModalBody>
                         <ModalFooter>
                             <Button color="primary" onClick={this.onContactView}>View</Button>{' '}
                             <Button color="primary" onClick={this.toggleForVUD}>Edit</Button>{' '}
-                            <Button color="primary" onClick={this.toggleForVUD}>Delete</Button>{' '}
+                            <Button color="primary" onClick={this.deleteContact}>Delete</Button>{' '}
                             <Button color="secondary" onClick={this.toggleForVUD}>Cancel</Button>
                         </ModalFooter>
                     </Modal>
                     <Modal isOpen={this.state.modalForNew} toggle={this.toggleForNew} className={this.props.className}>
                         <ModalHeader toggle={this.toggleForNew}>{this.state.selectedContact}</ModalHeader>
                         <ModalBody>
+                            <FormGroup>
+                                <Label htmlFor="contact_name">Contact Name</Label>
+                                <Input type="text" id="contact_name" value={this.state.contact_name} onChange={(e) => this.setState({ contact_name: e.target.value })} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="contact_mobile_number">Contact Mobile Number</Label>
+                                <Input type="text" id="contact_mobile_number" value={this.state.contact_mobile_number} onChange={(e) => this.setState({ contact_mobile_number: e.target.value })} />
+                            </FormGroup>
                         </ModalBody>
                         <ModalFooter>
-                            <Button color="primary" onClick={this.toggleForNew}>Submit</Button>{' '}
+                            <Button color="primary" onClick={this.addNewContact}>Submit</Button>{' '}
                             <Button color="secondary" onClick={this.toggleForNew}>Cancel</Button>
                         </ModalFooter>
                     </Modal>
@@ -107,4 +149,4 @@ class MainContactsComp extends Component {
         )
     }
 }
-export default connect(null,mapDispatchToProps)(MainContactsComp);
+export default connect(null, mapDispatchToProps)(MainContactsComp);
